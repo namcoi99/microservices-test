92974928283dc25c7d27faae71cfaee22f6fd6fc
namcoi99 <nam_1999_x@yahoo.com.vn>
2021-01-03 00:19:03 +0700

    add filter to product service

--- microservices-test/Product/product.js
+++ microservices-test/Product/product.js
@@ -174,6 +174,74 @@ sql.connect(config, (err, pool) => {
                 });
             }
         });
+
+        app.get('/price', async (req, res) => {
+            try {
+                const query = `
+                    SELECT * FROM Product
+                    WHERE Price BETWEEN ${req.query.from} AND ${req.query.to} 
+                    AND Category LIKE N'${req.query.category}'
+                    ORDER BY ${req.query.sortField ? req.query.sortField : 'Sold'} ${req.query.sortDirection > 0 ? 'ASC' : 'DESC'}
+                    OFFSET ${(req.query.pageNumber - 1) * req.query.pageSize} ROWS  
+                    FETCH NEXT ${req.query.pageSize} ROWS ONLY
+                `;
+                console.log(query);
+                const result = await new sql.Request().query(query);
+                const total = await new sql.Request().query(
+                    `
+                    SELECT COUNT(*) AS Total FROM Product
+                    WHERE Price BETWEEN ${req.query.from} AND ${req.query.to}
+                    AND Category LIKE N'${req.query.category}'
+                    `
+                );
+                // console.log(total);
+                res.status(200).json({
+                    success: true,
+                    data: {
+                        total: total.recordset[0].Total,
+                        recordset: result.recordset
+                    }
+                });
+            } catch (err) {
+                res.status(500).json({
+                    success: false,
+                    message: err.message
+                });
+            }
+        });
+        
+        app.get('/category', async (req, res) => {
+            try {
+                const query = `
+                    SELECT * FROM [Product]
+                    WHERE Category LIKE N'${req.query.category}'
+                    ORDER BY ${req.query.sortField ? req.query.sortField : 'Sold'} ${req.query.sortDirection > 0 ? 'ASC' : 'DESC'}
+                    OFFSET ${(req.query.pageNumber - 1) * req.query.pageSize} ROWS  
+                    FETCH NEXT ${req.query.pageSize} ROWS ONLY
+                `;
+                // console.log(query);
+                const result = await new sql.Request().query(query);
+                const total = await new sql.Request().query(
+                    `
+                    SELECT COUNT(*) AS Total FROM Product
+                    WHERE Category LIKE N'${req.query.category}'
+                    `
+                );
+                res.status(200).json({
+                    success: true,
+                    data: {
+                        total: total.recordset[0].Total,
+                        recordset: result.recordset
+                    }
+                });
+            } catch (err) {
+                res.status(500).json({
+                    success: false,
+                    message: err.message
+                });
+            }
+        });
+
         app.listen(process.env.PORT || 5005, (err) => {
             if (err) throw err;
             else console.log("Product service listen on port 5005...");
